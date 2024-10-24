eef150ecd238364c929888f577660858758d1fb1
henry <island_1123@163.com>
2024-10-24 10:40:49 +0800

    add initial file

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => {
+      // 移除所有class属性中包含'MDL_'的div元素
+      document.querySelectorAll('div[class*="MDL_"]').forEach(element => element.remove());
+
+      // 移除所有div元素，其data-testid属性为'beast-core-modal'
+      document.querySelectorAll('div[data-testid="beast-core-modal"]').forEach(element => element.remove());
+
+      // 移除所有div元素，其data-testid属性为'beast-core-modal-mask'
+      document.querySelectorAll('div[data-testid="beast-core-modal-mask"]').forEach(element => element.remove());
+    }
+  });
+});
